4a7c201b21b79c693e2f51bb1549d652ffa80817
leo bouyat <lbouyat@gaming.tech>
2026-02-24 13:00:45 +0100

    Update Wall.cpp

--- Soutien/Wall.cpp
+++ Soutien/Wall.cpp
@@ -4,6 +4,7 @@ Wall::Wall()
 {
 	rectangle.setFillColor({sf::Color::White});
 	rectangle.setSize({ 100.f, 100.f });
+	rectangle.setPosition({100.f, 100.f});
 }
 
 void Wall::Draw(sf::RenderWindow& window)
