2f460a7eca7a63fe4c815fdac7715a9c8666f498
llomboni <llomboni@gamin.tech>
2026-03-03 11:18:00 +0100

    commit player fix

--- Soutien/Player.h
+++ Soutien/Player.h
@@ -4,10 +4,12 @@
 class Player : protected Entity
 {
 public:
+	Player();
 	float health = 20;
 	std::string name = "Player";
 	int damage = 10;
 	void draw(sf::RenderWindow& window);
+	
 private: 
 	sf::RectangleShape rectangle;
 };
